0c91b0267ba5caa70fb57fe5ee59e7ec086dc116
Cole Arendt <cole.arendt@outlook.com>
2021-01-27 06:58:57 -0500

    regenerate main.py

--- main.py
+++ main.py
@@ -34,11 +34,9 @@ import boto3
 import tempfile
 import base64
 
-
 def _write_cafile(data: str) -> tempfile.NamedTemporaryFile:
     # protect yourself from automatic deletion
     cafile = tempfile.NamedTemporaryFile(delete=False)
-
     cadata_b64 = data
     cadata = base64.b64decode(cadata_b64)
     cafile.write(cadata)
@@ -79,3 +77,37 @@ api_client.create_namespaced_config_map(namespace='default', body=my_configmap)
 # use k8s_client.replace_namespaced_config_map
 #
 # If it already exists, you get a 409 conflict
+
+
+# ------------------------------------------------------
+# 6_bonus.py
+# ------------------------------------------------------
+def create_update_configmap(
+        k8s_client: kubernetes.client.CoreV1Api,
+        namespace: str,
+        configmap: kubernetes.client.V1ConfigMap
+) -> kubernetes.client.V1ConfigMap:
+    """
+    Try to create a new namespaced configmap, and fall back to replacing
+    a namespaced configmap if the create fails with a 409 (conflict)
+
+    :rtype: client.V1ConfigMap
+    :param k8s_client: The kubernetes.ApiClient object to use
+    :param namespace: The namespace to update a configmap within
+    :param configmap: The kubernetes.ConfigMap to apply
+    :return: The kubernetes.ConfigMap API response
+    """
+    try:
+        res = k8s_client.create_namespaced_config_map(namespace, configmap)
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 409:
+            # 409 conflict = it exists... try to replace instead
+            res = k8s_client.replace_namespaced_config_map(configmap.metadata['name'], namespace, configmap)
+        else:
+            raise e
+    return res
+
+
+create_update_configmap(api_client, namespace='default', configmap=my_configmap)
+
+api_client.delete_namespaced_config_map(namespace='default', name='my-configmap')
